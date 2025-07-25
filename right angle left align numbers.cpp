8e798b59cf3d99984ffd2d14571501d749c163c3
Jay Paygude <jaypaygude2@gmail.com>
2025-07-25 11:27:42 +0530

    Create right angle left align numbers.cpp

---
+++ right angle left align numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=1;
+    int row =4;
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (a<= 10)
+                cout << a++ << " ";
+        }
+        cout << endl;
+    }
+   return 0;
+}
